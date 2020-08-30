5d4dde5634ad56304d7d25ac2307b4b82849127c
flaviodiminuto <flavio.diminuto@outlook.com>
2020-08-30 03:53:31 -0300

    Adicao do cadastro de usuário

---
+++ MeuCabelereiroApplication/src/main/java/br/com/flaviodiminuto/MeuCabelereiroApplication/usuario/persistence/UsuarioRepository.java
@@ -0,0 +1,14 @@
+package br.com.flaviodiminuto.MeuCabelereiroApplication.usuario.persistence;
+
+import br.com.flaviodiminuto.MeuCabelereiroApplication.usuario.entity.UsuarioEntity;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends CrudRepository<UsuarioEntity, Long> {
+
+    @Query("select u from UsuarioEntity u where u.login = ?1")
+    UsuarioEntity findByLogin(String login);
+
+}
