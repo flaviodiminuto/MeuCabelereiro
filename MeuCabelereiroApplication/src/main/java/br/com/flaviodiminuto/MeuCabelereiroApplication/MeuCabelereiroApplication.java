b555065bf10ff3863940a1b8e5f9b23ed76f81d9
flaviodiminuto <flavio.diminuto@outlook.com>
2020-08-30 00:17:42 -0300

    Inicio da construcao da aplicacao

---
+++ MeuCabelereiroApplication/src/main/java/br/com/flaviodiminuto/MeuCabelereiroApplication/MeuCabelereiroApplication.java
@@ -0,0 +1,13 @@
+package br.com.flaviodiminuto.MeuCabelereiroApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuCabelereiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuCabelereiroApplication.class, args);
+	}
+
+}
