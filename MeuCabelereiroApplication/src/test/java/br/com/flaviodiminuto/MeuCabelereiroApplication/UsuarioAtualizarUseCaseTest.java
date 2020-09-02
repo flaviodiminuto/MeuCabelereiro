a0105e0daa0e5a954c5ce249cb3c1dcda7d0ad56
flaviodiminuto <flavio.diminuto@outlook.com>
2020-09-02 00:19:07 -0300

    [Test] Parametros com menos de oitro caracteres

--- MeuCabelereiroApplication/src/test/java/br/com/flaviodiminuto/MeuCabelereiroApplication/UsuarioAtualizarUseCaseTest.java
+++ MeuCabelereiroApplication/src/test/java/br/com/flaviodiminuto/MeuCabelereiroApplication/UsuarioAtualizarUseCaseTest.java
@@ -1,19 +1,29 @@
 package br.com.flaviodiminuto.MeuCabelereiroApplication;
 
 import br.com.flaviodiminuto.MeuCabelereiroApplication.usuario.usecase.UsuarioAtualizarUseCase;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Teste do Use Case de atualização de usuário")
 public class UsuarioAtualizarUseCaseTest {
+    UsuarioAtualizarUseCase usecase = new UsuarioAtualizarUseCase();
 
     @Test
+    @DisplayName("Todos os campos são válidos")
     public void camposValidos(){
-        UsuarioAtualizarUseCase usecase = new UsuarioAtualizarUseCase();
         List <String> campos = Arrays.asList("login1234", "senha1234", "novasenha");
         assertTrue(usecase.camposValidos(campos));
+    }
+
+    @Test
+    @DisplayName("Algum campo contém menos de oito caracteres - \uD83D\uDE31")
+    public void senhaComMenosDeOitoCaracteres(){
+        List<String> campos = Arrays.asList("novasenha", "senhavalida","invalid" );
+        assertFalse(usecase.camposValidos(campos));
     }
 }
